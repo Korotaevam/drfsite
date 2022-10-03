87f99c7d57950ca4b8c91d0f1a2fdbb1f03a8971
Korotaevam <kam888@mail.ru>
2022-10-03 15:12:49 +0300

    drf авторизации по JWT-токенам, pagination

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from women.views import *
 from rest_framework import routers
 
@@ -27,4 +29,7 @@ urlpatterns = [
     path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/auth/', include('djoser.urls')),          # new
     re_path(r'^auth/', include('djoser.urls.authtoken')),  # new
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
